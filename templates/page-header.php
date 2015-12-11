a761d8f644b36e4948d7c5140391058028f9aee9
tapps <t@tapps.email>
2015-12-11 02:12:16 -0600

    page header / featured image

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,18 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
+<?php
+
+if ( has_post_thumbnail() )
+
+?>
+<div class="page-header<?php
+		if ( has_post_thumbnail() ) :
+			$the_image_url = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+			echo ' has-image" style="background-image: url(' . esc_url( $the_image_url, 'web2' ) . ');"';
+		else :
+			echo '"';
+		endif;
+	?>
+>
 	<h1><?= Titles\title(); ?></h1>
 </div>
