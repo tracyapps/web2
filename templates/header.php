051e52e4c596aca84c9f3f4c870ec205703fd8d2
tapps <t@tapps.email>
2015-12-11 06:05:04 -0600

    util nav, custom walker for bootstrap & start of front page

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,17 @@
 <header class="banner">
 	<div class="container">
+		<nav class="nav-utility" id="utility-navigation">
+			<?php
+			if ( has_nav_menu( 'utility_navigation' ) ) :
+				wp_nav_menu( [
+					'theme_location'	=> 'utility_navigation',
+					'menu_class'		=> 'nav',
+					'fallback_cb'		=> 'wp_bootstrap_navwalker::fallback',
+					'walker'			=> new wp_bootstrap_navwalker()
+				] );
+			endif;
+			?>
+		</nav>
 		<a class="brand" href="<?= esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 
 	</div>
