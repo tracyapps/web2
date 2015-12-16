a76a68aaa9a725adcfa7e6f0a1b36112c84bb4ae
tapps <t@tapps.email>
2015-12-16 12:33:36 -0600

    rebuilding footer and utility

--- templates/header.php
+++ templates/header.php
@@ -18,7 +18,11 @@
 	<nav class="nav-primary" id="main-navigation">
 		<?php
 		if ( has_nav_menu( 'primary_navigation' ) ) :
-			wp_nav_menu( [ 'theme_location' => 'primary_navigation', 'menu_class' => 'nav' ] );
+			wp_nav_menu( [
+				'theme_location' => 'primary_navigation',
+				'menu_class' 	=> 'nav',
+				'depth'			=> 1
+			] );
 		endif;
 		?>
 	</nav>
