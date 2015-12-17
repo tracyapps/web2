88deed7b5dcf395c1dc5537d34c544162afcebe1
tapps <t@tapps.email>
2015-12-17 02:49:23 -0600

    front page styles etc.

--- templates/content-page.php
+++ templates/content-page.php
@@ -14,77 +14,83 @@
 	//print_r($box_group_1_array);
 	?>
 
-	<section class="one-third box-one">
-		<div class="row block-center">
-			<?php
+	<div class="row three-boxes">
+		<hr />
+
+		<section class="one-third box-one">
+			<div class="wrapper block-center">
+				<?php
 				printf(
 					'<a href="%s" class="centered-icon"><img class="iconic" data-src="%s.svg"></a>',
 					esc_url( get_page_link( $box_group_1_array[ 'box1-post' ] ), 'web2' ),
 					esc_url( $icon_dir . 'svg/smart/' . $box_group_1_array[ 'box1-icon' ], 'web2' )
-					);
-			?>
-		</div>
-		<div class="row">
-			<h3>
-				<a href="<?php echo esc_url( get_page_link( $box_group_1_array[ 'box1-post' ] ), 'web2' ) ?>">
-					<?php esc_html_e( $box_group_1_array[ 'box1-title' ], 'web2' ); ?>
-				</a>
-			</h3>
-			<p>
-				<?php
-				echo wp_kses_post( $box_group_1_array[ 'box1-excerpt' ], 'web2' );
+				);
 				?>
-			</p>
-		</div>
-	</section>
+			</div>
+			<div class="wrapper">
+				<h3>
+					<a href="<?php echo esc_url( get_page_link( $box_group_1_array[ 'box1-post' ] ), 'web2' ) ?>">
+						<?php esc_html_e( $box_group_1_array[ 'box1-title' ], 'web2' ); ?>
+					</a>
+				</h3>
+				<p>
+					<?php
+					echo wp_kses_post( $box_group_1_array[ 'box1-excerpt' ], 'web2' );
+					?>
+				</p>
+			</div>
+		</section>
 
-	<section class="one-third box-two">
-		<div class="row block-center">
-			<?php
-			printf(
-				'<a href="%s" class="centered-icon"><img class="iconic" data-src="%s.svg"></a>',
-				esc_url( get_page_link( $box_group_2_array[ 'box2-post' ] ), 'web2' ),
-				esc_url( $icon_dir . 'svg/smart/' . $box_group_2_array[ 'box2-icon' ], 'web2' )
-			);
-			?>
-		</div>
-		<div class="row">
-			<h3>
-				<a href="<?php echo esc_url( get_page_link( $box_group_2_array[ 'box2-post' ] ), 'web2' ) ?>">
-					<?php esc_html_e( $box_group_2_array[ 'box2-title' ], 'web2' ); ?>
-				</a>
-			</h3>
-			<p>
+		<section class="one-third box-two">
+			<div class="wrapper block-center">
 				<?php
-				echo wp_kses_post( $box_group_2_array[ 'box2-excerpt' ], 'web2' );
+				printf(
+					'<a href="%s" class="centered-icon"><img class="iconic" data-src="%s.svg"></a>',
+					esc_url( get_page_link( $box_group_2_array[ 'box2-post' ] ), 'web2' ),
+					esc_url( $icon_dir . 'svg/smart/' . $box_group_2_array[ 'box2-icon' ], 'web2' )
+				);
 				?>
-			</p>
-		</div>
-	</section>
+			</div>
+			<div class="wrapper">
+				<h3>
+					<a href="<?php echo esc_url( get_page_link( $box_group_2_array[ 'box2-post' ] ), 'web2' ) ?>">
+						<?php esc_html_e( $box_group_2_array[ 'box2-title' ], 'web2' ); ?>
+					</a>
+				</h3>
+				<p>
+					<?php
+					echo wp_kses_post( $box_group_2_array[ 'box2-excerpt' ], 'web2' );
+					?>
+				</p>
+			</div>
+		</section>
 
-	<section class="one-third box-three">
-		<div class="row block-center">
-			<?php
-			printf(
-				'<a href="%s" class="centered-icon"><img class="iconic" data-src="%s.svg"></a>',
-				esc_url( get_page_link( $box_group_3_array[ 'box3-post' ] ), 'web2' ),
-				esc_url( $icon_dir . 'svg/smart/' . $box_group_3_array[ 'box3-icon' ], 'web2' )
-			);
-			?>
-		</div>
-		<div class="row">
-			<h3>
-				<a href="<?php echo esc_url( get_page_link( $box_group_3_array[ 'box3-post' ] ), 'web2' ) ?>">
-					<?php esc_html_e( $box_group_3_array[ 'box3-title' ], 'web2' ); ?>
-				</a>
-			</h3>
-			<p>
+		<section class="one-third box-three">
+			<div class="wrapper block-center">
 				<?php
-				echo wp_kses_post( $box_group_3_array[ 'box3-excerpt' ], 'web2' );
+				printf(
+					'<a href="%s" class="centered-icon"><img class="iconic" data-src="%s.svg"></a>',
+					esc_url( get_page_link( $box_group_3_array[ 'box3-post' ] ), 'web2' ),
+					esc_url( $icon_dir . 'svg/smart/' . $box_group_3_array[ 'box3-icon' ], 'web2' )
+				);
 				?>
-			</p>
-		</div>
-	</section>
+			</div>
+			<div class="wrapper">
+				<h3>
+					<a href="<?php echo esc_url( get_page_link( $box_group_3_array[ 'box3-post' ] ), 'web2' ) ?>">
+						<?php esc_html_e( $box_group_3_array[ 'box3-title' ], 'web2' ); ?>
+					</a>
+				</h3>
+				<p>
+					<?php
+					echo wp_kses_post( $box_group_3_array[ 'box3-excerpt' ], 'web2' );
+					?>
+				</p>
+			</div>
+		</section>
+	</div>
+	<hr />
+
 	<?php
 	endif;
 ?>
