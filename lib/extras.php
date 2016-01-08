8f4b05c7530bde122e963333c50f103245f763cd
tapps <t@tapps.email>
2016-01-08 04:41:10 -0600

    one last try...

--- lib/extras.php
+++ lib/extras.php
@@ -39,3 +39,18 @@ add_filter( 'excerpt_more', __NAMESPACE__ . '\\excerpt_more' );
  * buddypress things
  */
 
+function bp_guest_redirect() {
+	global $bp;
+	if ( bp_is_activity_component()
+		|| bp_is_groups_component()
+		/*|| bbp_is_single_forum()*/
+		|| bp_is_forums_component()
+		|| bp_is_blogs_component()
+		|| bp_is_page( BP_MEMBERS_SLUG ) ) {
+		// enter the slug or component conditional here
+		if(!is_user_logged_in()) {
+			wp_redirect( get_option('siteurl') . '/register' );
+		}
+	}
+}
+add_filter( 'get_header', 'bp_guest_redirect', 1 );
