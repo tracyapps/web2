e9ae5b40a42d46fd95b281aa591a258eac2b47ca
tapps <t@tapps.email>
2016-01-08 03:53:15 -0600

    ok forget it.

--- lib/extras.php
+++ lib/extras.php
@@ -34,21 +34,3 @@ function excerpt_more() {
 
 add_filter( 'excerpt_more', __NAMESPACE__ . '\\excerpt_more' );
 
-
-add_filter('logout_url', 'web2_logout_url',100,2);
-function web2_logout_url( $logout_url, $redirect) {
-	//simply ignore the redirect and set it to the main domain
-
-	$redirect = get_blog_option( SITE_ID_CURRENT_SITE, 'siteurl' );
-
-
-	$args = array( 'action' => 'logout' );
-	if ( !empty($redirect) ) {
-		$args['redirect_to'] = $redirect;
-	}
-
-	$logout_url = add_query_arg($args, site_url('wp-login.php', 'login'));
-	$logout_url = wp_nonce_url( $logout_url, 'log-out' );
-
-	return $logout_url;
-}
