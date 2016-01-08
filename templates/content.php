5805d6710cc323b35be210778ab7dae9b703d060
tapps <t@tapps.email>
2016-01-08 11:50:39 -0600

    fix buddypress error and add category tags

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,10 @@
 <article <?php post_class(); ?>>
+	<div class="post-category-label">
+		<?php $categories = get_the_category();
+		if ( ! empty( $categories ) ) {
+			echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+		} ?>
+	</div>
 	<div class="post-thumbnail-column">
 		<?php if ( has_post_thumbnail() ) :
 			echo '<a href="' . get_permalink() . '">';
